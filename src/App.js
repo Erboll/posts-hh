e69d1f07dd2a34ee998f612292837bfb2d1eb79d
Erbol Betenov <betenov64@mail.ru>
2023-05-26 22:28:53 +0600

    Add Routes and configured routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import AboutUser from "./pages/AboutUser/AboutUser";
+import AboutMe from "./pages/AboutMe/AboutMe";
+import Navbar from "./components/Navbar/Navbar";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about-user" element={<AboutUser />} />
+        <Route path="/about-me" element={<AboutMe />} />
+        <Route path="*" element={<div>NOT FOUND</div>} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
