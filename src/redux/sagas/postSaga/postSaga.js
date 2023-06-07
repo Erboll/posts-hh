98c888fee4016cb8daa1754ad2ccc15b737bad64
Erbol Betenov <betenov64@mail.ru>
2023-06-08 00:08:25 +0600

    Add error validation

--- src/redux/sagas/postSaga/postSaga.js
+++ src/redux/sagas/postSaga/postSaga.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { call, put, take, takeEvery, takeLatest } from "redux-saga/effects";
+import { call, put, takeEvery, takeLatest } from "redux-saga/effects";
 import {
   LOAD_COMMENTS,
   LOAD_COMMENTS_SUCCESS,
@@ -12,34 +12,50 @@ import {
 } from "../../constants";
 
 export function* loadDetailsUser({ payload }) {
-  const data = yield call(
-    axios.get,
-    "https://jsonplaceholder.typicode.com/users/" + payload
-  );
-  yield put({ type: LOAD_USER_DETAILS_SUCCESS, payload: data.data });
+  try {
+    const data = yield call(
+      axios.get,
+      "https://jsonplaceholder.typicode.com/users/" + payload
+    );
+    yield put({ type: LOAD_USER_DETAILS_SUCCESS, payload: data.data });
+  } catch (e) {
+    console.log(e.message);
+  }
 }
 export function* loadPostsOfUser({ payload }) {
-  const data = yield call(
-    axios.get,
-    "https://jsonplaceholder.typicode.com/users/" + payload + "/posts"
-  );
-  yield put({ type: LOAD_USER_DETAILS_SUCCESS_POSTS, payload: data.data });
+  try {
+    const data = yield call(
+      axios.get,
+      "https://jsonplaceholder.typicode.com/users/" + payload + "/posts"
+    );
+    yield put({ type: LOAD_USER_DETAILS_SUCCESS_POSTS, payload: data.data });
+  } catch (e) {
+    console.log(e.message);
+  }
 }
 
 export function* loadCommentsOfPosts({ payload }) {
-  const data = yield call(
-    axios.get,
-    "https://jsonplaceholder.typicode.com/comments?postId=" + payload
-  );
-  yield put({ type: LOAD_COMMENTS_SUCCESS, payload: data.data });
+  try {
+    const data = yield call(
+      axios.get,
+      "https://jsonplaceholder.typicode.com/commentss?postId=" + payload
+    );
+    yield put({ type: LOAD_COMMENTS_SUCCESS, payload: data.data });
+  } catch (e) {
+    console.log(e.message);
+  }
 }
 
 export function* loadPosts() {
-  const data = yield call(
-    axios.get,
-    "https://jsonplaceholder.typicode.com/posts"
-  );
-  yield put({ type: LOAD_POSTS_SUCCESS, payload: data.data });
+  try {
+    const data = yield call(
+      axios.get,
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    yield put({ type: LOAD_POSTS_SUCCESS, payload: data.data });
+  } catch (e) {
+    console.log(e.message);
+  }
 }
 
 export function* clickCommentSaga() {
